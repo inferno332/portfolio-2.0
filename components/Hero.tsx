e4eada7cddb8adbe5d6782f8b9d2889a4ec842ab
Cong Khoa Huynh <bhuynhcongkhoa@gmail.com>
2023-04-26 21:26:20 +0700

    typewriter install, create hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Cursor,useTypewriter } from "react-simple-typewriter";
+
+type Props = {};
+
+function Hero({}: Props) {
+  return <div>
+
+  </div>;
+}
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-
-      {/* Header */}
-      <Header/>
+      <Header />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About */}
 
@@ -22,7 +24,6 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact Me */}
-
     </main>
   );
 }
