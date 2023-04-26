606d007017e01d74571a3154b62e9147ccade5a0
Cong Khoa Huynh <bhuynhcongkhoa@gmail.com>
2023-04-26 23:17:28 +0700

    hero background | About section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,49 @@
 import React from "react";
-import { Cursor,useTypewriter } from "react-simple-typewriter";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircle from "./BackgroundCircle";
+import Link from "next/link";
 
 type Props = {};
 
 function Hero({}: Props) {
-  return <div>
-
-  </div>;
+  const [text] = useTypewriter({
+    words: ["Hi, The Name's Khoa", "Guy-who-loves-Coffee.jsx", "<AndCoding />"],
+    loop: 0,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircle />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://i.pravatar.cc/300"
+        alt="me"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7ab0a" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,27 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Experience from "@/components/Experience";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap- overflow-scroll z-0">
       <Header />
 
-      {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
-      {/* Experience */}
+      <section id="about" className="snap-center">
+        <Experience />
+      </section>
 
       {/* Skills */}
 
